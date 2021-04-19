80bf297c26d8a635ad9478aa182d7fba63a5996a
Joe Pedroza <jpedroza50@yahoo.com>
2021-04-18 20:06:07 -0500

    Added second challenge part to MechaCar.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,4 +9,22 @@ summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,
 
 
 
+# Import and read in the Suspension_Coil.csv file as a table
+mecha_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
+
+# Create a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI),
+                                          Median_PSI=median(PSI),
+                                          Var_PSI=var(PSI),
+                                          Std_Dev_PSI=sd(PSI),
+                                          Num_Coil=n(), .groups = 'keep') 
+# Create a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot.                                                                
+lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                         Median_PSI=median(PSI),
+                                                                       Var_PSI=var(PSI),
+                                                                         Std_Dev_PSI=sd(PSI),
+                                                                         Num_Coil=n(), .groups = 'keep')                                      
+
+
+
 
--- README.md
+++ README.md
@@ -9,3 +9,15 @@ The three variables that had an insignificant amount of variance is vehicle weig
 The p-value of 5.35e-11 is less than 0.05% in a positive direction. There is sufficient evidence to reject the null hypothesis.
 - Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
 Our R-squared value is 71%, which means roughly ~71% of the time the model will predict mpg values correctly. There is a 29% variance that other factors not accounted for impact the mpg analysis prediction.
+
+
+## Summary Statistics on Suspension Coils
+
+- The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
+According to the summarization of our data, the variance analysis of the suspension coils as a whole do meet the design specifications of not to exceed 100 PSI.  Looking at our summary, the variance of PSI is 62.29 and is under the requirement of 100 PSI.
+![MPG Prediction by Linear Regression](images/total_summary.png)
+
+Doing variance analysis on a per lot basis, we see that lot3 is over the design specifications of exceeding 100 PSI
+![MPG Prediction by Linear Regression](images/lot_summary.png)
+
+
